11cd669520df163c158a91deac8f48808de9c8a6
samuelstefanelli <47322057+samuelstefanelli@users.noreply.github.com>
2024-01-17 19:50:40 -0500

    Update Project_One_Upload.py
    Add new line at end of statement.
    Create new line of text.

--- Project_One_Upload.py
+++ Project_One_Upload.py
@@ -2,3 +2,9 @@
 #By Sam Stefanelli
 
 print("COP2002 Spring 2024")
+
+#Addition of required text line
+print("I'm understanding how to use Github to keep track of changes!")
+
+#Addition of blank line at end of file per instructions
+print()
